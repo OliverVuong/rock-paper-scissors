fa34580de582a25f5e9de7ce63b5d0fe6c5e7179
Oliver Vuong <vuongoliver@gmail.com>
2023-01-23 03:33:12 -0800

    Add UI that tracks win counts

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ function getComputerChoice(){
     }
 }
 
+let playerWins = 0;
+let computerWins = 0;
+
 function getResults(playerSelection, computerSelection) {
   // your code here!
 
@@ -20,30 +23,33 @@ function getResults(playerSelection, computerSelection) {
     if(playerSelection === "rock" && computerSelection === "rock"){
         return "Result: Tie";
     } else if(playerSelection === "rock" && computerSelection === "paper"){
+        computerWins++
         return "Result: You Lose! Paper beats Rock";
     } else if(playerSelection === "rock" && computerSelection === "scissors"){
+        playerWins++;
         return "Result: You Win! Rock beats Scissors";
     } 
     
     else if(playerSelection === "paper" && computerSelection === "rock"){
+        playerWins++;
         return "Result: You Win! Paper beats Rock";
     } else if(playerSelection === "paper" && computerSelection === "paper"){
         return "Result: Tie";
     } else if(playerSelection === "paper" && computerSelection === "scissors"){
+        computerWins++
         return "Result: You Lose! Scissors beats Paper";
     }
     
     else if(playerSelection === "scissors" && computerSelection === "rock"){
+        computerWins++
         return "Result: You Lose! Rock beats Scissors";
     } else if(playerSelection === "scissors" && computerSelection === "paper"){
+        playerWins++;
         return "Result: You Win! Scissors beats Paper";
     } else { //scissors vs scissors
         return "Result: Tie";
     }
 }
-
-//let playerSelection = "rock";
-//let computerSelection = "rock";
 
 const buttons = document.querySelectorAll('button');
 buttons.forEach(button => button.addEventListener('click', playRound));
@@ -51,11 +57,17 @@ buttons.forEach(button => button.addEventListener('click', playRound));
 const compSelectionDisplay = document.querySelector('.computerSelection');
 const result = document.querySelector('.result');
 
+const playerWinDisplay = document.querySelector('.playerWins');
+const computerWinDisplay = document.querySelector('.computerWins');
+
+
 function playRound(e){
     let playerSelection = this.getAttribute('class');
     let computerSelection = getComputerChoice();
     result.textContent = getResults(playerSelection, computerSelection);
     compSelectionDisplay.textContent = "Opponent: " + computerSelection;
+    playerWinDisplay.textContent = "Player Win Count: " + playerWins;
+    computerWinDisplay.textContent = "Computer Win Count: " + computerWins;
 }
 
 function game(){
